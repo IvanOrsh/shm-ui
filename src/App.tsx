f5a8a285e3f0cbefaf89e6c7d7042179defc38c6
IvanOrsh <ksironov@mail.ru>
2023-12-11 14:50:03 +0300

    refactor: extract Layout to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,34 @@
 import Container from "./components/site/Container";
-import Navbar from "./components/site/Navbar";
+import Layout from "./components/site/Layout";
 import Section from "./components/site/Section";
 
 function App() {
   return (
-    <div className="relative min-h-screen bg-white">
-      <Navbar />
+    <Layout>
+      <Container description="Container description" title="Container title">
+        <Section description="Section description" title="Section title">
+          <div>component 1</div>
+          <div>component 2</div>
+        </Section>
 
-      {/* Main content area */}
-      <main className="p-10 sm:px-16 lg:px-44">
-        <div className="mx-auto max-w-3xl space-y-20">
-          <Container
-            description="Container description"
-            title="Container title"
-          >
-            <Section description="Section description" title="Section title">
-              <div>component 1</div>
-              <div>component 2</div>
-            </Section>
+        <Section description="Section description" title="Section title">
+          <div>component 1</div>
+          <div>component 2</div>
+        </Section>
+      </Container>
 
-            <Section description="Section description" title="Section title">
-              <div>component 1</div>
-              <div>component 2</div>
-            </Section>
-          </Container>
+      <Container description="Container description" title="Container title">
+        <Section description="Section description" title="Section title">
+          <div>component 1</div>
+          <div>component 2</div>
+        </Section>
 
-          <Container
-            description="Container description"
-            title="Container title"
-          >
-            <Section description="Section description" title="Section title">
-              <div>component 1</div>
-              <div>component 2</div>
-            </Section>
-
-            <Section description="Section description" title="Section title">
-              <div>component 1</div>
-              <div>component 2</div>
-            </Section>
-          </Container>
-        </div>
-      </main>
-    </div>
+        <Section description="Section description" title="Section title">
+          <div>component 1</div>
+          <div>component 2</div>
+        </Section>
+      </Container>
+    </Layout>
   );
 }
 
---
+++ src/components/site/Layout.tsx
@@ -0,0 +1,18 @@
+import { PropsWithChildren } from "react";
+
+import Navbar from "./Navbar";
+
+export default function Layout(props: PropsWithChildren) {
+  const { children } = props;
+
+  return (
+    <div className="relative min-h-screen bg-white">
+      <Navbar />
+
+      {/* Main content area */}
+      <main className="p-10 sm:px-16 lg:px-44">
+        <div className="mx-auto max-w-3xl space-y-20">{children}</div>
+      </main>
+    </div>
+  );
+}
