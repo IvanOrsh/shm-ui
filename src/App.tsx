9f8cae41fe3f731509ee6abd3e239fc70ab7d4db
IvanOrsh <ksironov@mail.ru>
2023-12-11 21:33:59 +0300

    feat: button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/library/Button";
 import Container from "./components/site/Container";
 import Layout from "./components/site/Layout";
 import Section from "./components/site/Section";
@@ -5,15 +6,113 @@ import Section from "./components/site/Section";
 function App() {
   return (
     <Layout>
-      <Container description="Container description" title="Container title">
-        <Section description="Section description" title="Section title">
-          <div>component 1</div>
-          <div>component 2</div>
+      <Container
+        title="Button"
+        description="A button is a common graphical user interface (GUI) widget that allows users to interact with a software application or website. When a user clicks or taps on a button, it triggers an action or performs a specific task, such as submitting a form, navigating to a different page, or executing a function in the underlying code."
+      >
+        <Section
+          title="Primary"
+          description="Primary actions should be obvious. Solid, high contrast background colors work great here."
+        >
+          {["sm", "md", "lg", "xl"].map((size) => (
+            <Button
+              key={size}
+              intent="primary"
+              size={size as "sm" | "md" | "lg" | "xl"}
+            >
+              Primary: {size}
+            </Button>
+          ))}
+          <Button intent="primary" disabled>
+            Primary: disabled
+          </Button>
         </Section>
 
-        <Section description="Section description" title="Section title">
-          <div>component 1</div>
-          <div>component 2</div>
+        <Section
+          title="Secondary"
+          description="Secondary actions actions should be clear but not prominent. Outline styles or lower contrast background colors are great options."
+        >
+          {["sm", "md", "lg", "xl"].map((size) => (
+            <Button
+              key={size}
+              intent="secondary"
+              size={size as "sm" | "md" | "lg" | "xl"}
+            >
+              Secondary: {size}
+            </Button>
+          ))}
+          <Button intent="secondary" disabled>
+            Secondary: disabled
+          </Button>
+        </Section>
+
+        <Section
+          title="Tertiary"
+          description="Tertiary actions should be discoverable but unobtrusive. Styling these actions like links is usually the best approach"
+        >
+          {["sm", "md", "lg", "xl"].map((size) => (
+            <Button
+              key={size}
+              intent="tertiary"
+              size={size as "sm" | "md" | "lg" | "xl"}
+            >
+              Tertiary: {size}
+            </Button>
+          ))}
+          <Button intent="tertiary" disabled>
+            Tertiary: disabled
+          </Button>
+        </Section>
+
+        <Section
+          title="Destructive"
+          description="Destructive actions typically result in the removal of an item, and may result in an undesirable consequence."
+        >
+          {["sm", "md", "lg", "xl"].map((size) => (
+            <Button
+              key={size}
+              intent="destructive"
+              size={size as "sm" | "md" | "lg" | "xl"}
+            >
+              Destructive: {size}
+            </Button>
+          ))}
+          <Button intent="destructive" disabled>
+            Destructive: disabled
+          </Button>
+        </Section>
+
+        <Section
+          title="Icon Buttons"
+          description="Icon buttons are great for actions that require an icon, such as a back or forward button."
+        >
+          {["sm", "md", "lg", "xl"].map((size) =>
+            ["primary", "secondary", "tertiary"].map((intent) => (
+              <Button
+                key={size}
+                intent={intent as "primary" | "secondary" | "tertiary"}
+                btnType="icon"
+                size={size as "sm" | "md" | "lg" | "xl"}
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 12h14m-7-7v14"
+                  />
+                </svg>
+              </Button>
+            ))
+          )}
         </Section>
       </Container>
 
---
+++ src/components/library/Button.tsx
@@ -0,0 +1,142 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+const button = cva(
+  [
+    "inline-flex",
+    "items-center",
+    "justify-center",
+    "gap-2",
+
+    "rounded-md",
+
+    "focus:outline-none",
+    "focus-visible:ring-4",
+    "focus:ring-2",
+    "focus:ring-offset-2",
+
+    "disabled:cursor-not-allowed",
+    "disabled:opacity-50",
+  ],
+  {
+    variants: {
+      intent: {
+        primary: [
+          "bg-theme-600",
+          "text-theme-50",
+
+          "hover:bg-theme-500",
+          "active:bg-theme-700",
+          "focus-visible:bg-theme-600",
+
+          "disabled:bg-theme-50",
+          "disabled:bg-theme-200",
+          "disabled:text-theme-400",
+
+          "border",
+          "border-transparent",
+        ],
+
+        secondary: [
+          "bg-theme-base",
+          "text-theme-600",
+
+          "border-2",
+          "border-theme-400",
+
+          "hover:bg-theme-100",
+          "hover:border-theme-700",
+
+          "active:bg-theme-200",
+
+          "disabled:bg-theme-base",
+          "disabled:border-theme-200",
+          "disabled:text-theme-400",
+        ],
+
+        tertiary: [
+          "bg-theme-base",
+          "text-theme-600",
+
+          "hover:bg-theme-50",
+
+          "active:bg-theme-100",
+
+          "disabled:bg-theme-base",
+          "disabled:border-theme-200",
+          "disabled:text-theme-400",
+        ],
+
+        destructive: [
+          "bg-red-500",
+          "text-red-50",
+
+          "hover:bg-red-600",
+
+          "active:bg-red-700",
+
+          "focus-visible:bg-red-500",
+
+          "disabled:bg-red-400",
+          "disabled:text-red-300",
+        ],
+      },
+      size: {
+        sm: ["text-sm", "py-1.5", "min-h-9", "px-3"],
+        md: ["text-sm", "py-2", "min-h-10", "px-5"],
+        lg: ["text-md", "py-2.5", "min-h-11", "px-5"],
+        xl: ["text-md", "py-3", "min-h-12", "px-6"],
+      },
+      btnType: {
+        button: "",
+        icon: ["px-0", "rounded-full"],
+      },
+    },
+    compoundVariants: [
+      {
+        btnType: "icon",
+        size: "sm",
+        class: "h-10 w-10",
+      },
+      {
+        btnType: "icon",
+        size: "md",
+        class: "h-11 w-11",
+      },
+      {
+        btnType: "icon",
+        size: "lg",
+        class: "h-12 w-12",
+      },
+      {
+        btnType: "icon",
+        size: "xl",
+        class: "h-[52px] w-[52px]",
+      },
+    ],
+    defaultVariants: {
+      intent: "primary",
+      size: "md",
+      btnType: "button",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof button> {}
+
+export const Button: React.FC<ButtonProps> = ({
+  intent,
+  size,
+  btnType,
+  className,
+  ...props
+}) => {
+  return (
+    <button
+      className={twMerge(button({ intent, size, btnType, className }))}
+      {...props}
+    />
+  );
+};
