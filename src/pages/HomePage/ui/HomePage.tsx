b55fa9b42b0a3e0a42fa83a356347b94c175c660
IvanOrsh <ksironov@mail.ru>
2023-12-14 00:39:39 +0300

    feat: implement links to pages

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,15 +1,43 @@
-import { PropsWithChildren } from "react";
+import { RxButton } from "react-icons/rx";
+import { Link } from "react-router-dom";
+
+import { BlockLinkCard } from "../../../components/library/Card";
 import Container from "../../../components/site/Container";
 
-export default function HomePage(props: PropsWithChildren) {
-  const { children } = props;
-
+export default function HomePage() {
   return (
     <Container
       title="shm's Components"
       description="A collection of my customized components. Built with React and Tailwind CSS."
     >
-      {children}
+      <div className="mt-8 grid gap-4 text-black dark:text-white sm:grid-cols-2 sm:gap-6">
+        <BlockLinkCard
+          CustomLinkComponent={Link}
+          url="button"
+          text="Button"
+          widthLimit="full"
+        >
+          <RxButton className="h-10 w-10" />
+        </BlockLinkCard>
+
+        <BlockLinkCard
+          CustomLinkComponent={Link}
+          url="card"
+          text="Card"
+          widthLimit="full"
+        >
+          <RxButton className="h-10 w-10" />
+        </BlockLinkCard>
+
+        <BlockLinkCard
+          CustomLinkComponent={Link}
+          url="input"
+          text="Input"
+          widthLimit="full"
+        >
+          <RxButton className="h-10 w-10" />
+        </BlockLinkCard>
+      </div>
     </Container>
   );
 }
